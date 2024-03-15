e81dfbe14ff36877c1ef65b07c5009be645977de
Dmitriy <dmitriyalieksieiev@gmail.com>
2024-03-15 05:56:39 +0200

    Add base directores.

---
+++ cmd/goshop/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/donseba/go-htmx"
+	"github.com/donseba/go-htmx/middleware"
+)
+
+type App struct {
+	htmx *htmx.HTMX
+}
+
+func main() {
+	// new app with htmx instance
+	app := &App{
+		htmx: htmx.New(),
+	}
+
+	mux := http.NewServeMux()
+	// wrap the htmx example middleware around the http handler
+
+	mux.Handle("/", http.FileServer(http.Dir("")))
+	mux.Handle("/test", middleware.MiddleWare(http.HandlerFunc(app.Home)))
+
+	err := http.ListenAndServe(":3000", mux)
+	log.Fatal(err)
+}
+
+func (a *App) Home(w http.ResponseWriter, r *http.Request) {
+	// initiate a new htmx handler
+	h := a.htmx.NewHandler(w, r)
+
+	// check if the request is a htmx request
+	if h.IsHxRequest() {
+		// do something
+	}
+
+	// check if the request is boosted
+	if h.IsHxBoosted() {
+		// do something
+	}
+
+	// check if the request is a history restore request
+	if h.IsHxHistoryRestoreRequest() {
+		// do something
+	}
+
+	// check if the request is a prompt request
+	if h.RenderPartial() {
+		// do something
+	}
+
+	// set the headers for the response, see docs for more options
+	swap := htmx.NewSwap().Swap().ScrollBottom()
+	h.ReSwapWithObject(swap)
+	// write the output like you normally do.
+	// check the inspector tool in the browser to see that the headers are set.
+	fmt.Println("response")
+	_, _ = h.Write([]byte("OK"))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Alieksieiev0/goshop
+
+go 1.22.1
+
+require github.com/donseba/go-htmx v1.7.0 // indirect
